fe007b02e1836b6e3b328820cc1b91f915eec2e9
Cao Van Bi <c-bi@ruby-dev.jp>
2023-05-19 16:40:53 +0900

    Add chapter 10

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template "static_pages/home"
@@ -14,5 +18,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    get help_path
+    assert_select "title", full_title("Help")
+
+    log_in_as(@user)
+    get users_path
+    assert_select "title", full_title("All users")
   end
 end
